bd5a6824cbc050a5bc441da93f3cf88c002a5e67
Roberto Tauille <robertotauille@gmail.com>
2022-12-13 13:31:22 -0300

    refactor: improves remix init

--- remix.init/index.cjs
+++ remix.init/index.cjs
@@ -10,43 +10,47 @@ const inquirer = require("inquirer");
 
 // clear();
 
-inquirer
-  .prompt([
-    {
-      name: "projectName",
-      message: "Qual é o nome do projeto?",
-    },
-    {
-      name: "restApiUrl",
-      message: "Qual é o endpoint da API REST?",
-    },
-    {
-      name: "graphqlApiUrl",
-      message: "Qual é o endpoint da API Graphql?",
-    },
-  ])
-  .then((answers) => {
-    figlet.text(
-      "Sysgaming",
+const setup = () => {
+  inquirer
+    .prompt([
       {
-        font: "ANSI Shadow",
-        horizontalLayout: "default",
-        verticalLayout: "default",
-        width: 80,
-        whitespaceBreak: true,
+        name: "projectName",
+        message: "Qual é o nome do projeto?",
       },
+      {
+        name: "restApiUrl",
+        message: "Qual é o endpoint da API REST?",
+      },
+      {
+        name: "graphqlApiUrl",
+        message: "Qual é o endpoint da API Graphql?",
+      },
+    ])
+    .then((answers) => {
+      figlet.text(
+        "Sysgaming",
+        {
+          font: "ANSI Shadow",
+          horizontalLayout: "default",
+          verticalLayout: "default",
+          width: 80,
+          whitespaceBreak: true,
+        },
 
-      function (err, data) {
-        if (err) {
-          console.log("😵 Ops.. algo saiu mal.");
-          return;
+        function (err, data) {
+          if (err) {
+            console.log("😵 Ops.. algo saiu mal.");
+            return;
+          }
+
+          console.log(chalk.blue(data));
+
+          console.log("Nome do projeto:", chalk.green(answers.projectName));
+          console.log("API REST:", chalk.cyan(answers.restApiUrl));
+          console.log("API Graphql:", chalk.magenta(answers.restApiUrl));
         }
+      );
+    });
+};
 
-        console.log(chalk.blue(data));
-
-        console.log("Nome do projeto:", chalk.green(answers.projectName));
-        console.log("API REST:", chalk.cyan(answers.restApiUrl));
-        console.log("API Graphql:", chalk.magenta(answers.restApiUrl));
-      }
-    );
-  });
+module.exports = setup;
