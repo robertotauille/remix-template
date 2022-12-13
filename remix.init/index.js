e34b026855202b16a1bae3f54782801c2d4c1f39
Roberto Tauille <robertotauille@gmail.com>
2022-12-13 13:12:36 -0300

    refactor: improves remix init

--- remix.init/index.js
+++ remix.init/index.js
@@ -1,35 +1,34 @@
-const inquirer = require("inquirer");
-const figlet = require("figlet");
-const chalk = require("chalk");
-const clear = require("clear");
+import { prompt } from "inquirer";
+import { text } from "figlet";
+import { green, blue, magenta } from "chalk";
+
+import clear from "clear";
 
 clear();
 
-inquirer
-  .prompt([
-    {
-      name: "projectName",
-      message: "Qual é o nome do projeto?",
-    },
-    {
-      name: "restApiUrl",
-      message: "Qual é o endpoint da API REST?",
-    },
-    {
-      name: "graphqlApiUrl",
-      message: "Qual é o endpoint da API Graphql?",
-    },
-  ])
-  .then((answers) => {
-    figlet.text("Sysgaming", {
-      font: "ANSI Shadow",
-      horizontalLayout: "default",
-      verticalLayout: "default",
-      width: 80,
-      whitespaceBreak: true,
-    });
-
-    console.info("Nome do projeto:", chalk.green(answers.projectName));
-    console.info("API REST:", chalk.blue(answers.restApiUrl));
-    console.info("API Graphql:", chalk.magenta(answers.restApiUrl));
+prompt([
+  {
+    name: "projectName",
+    message: "Qual é o nome do projeto?",
+  },
+  {
+    name: "restApiUrl",
+    message: "Qual é o endpoint da API REST?",
+  },
+  {
+    name: "graphqlApiUrl",
+    message: "Qual é o endpoint da API Graphql?",
+  },
+]).then((answers) => {
+  text("Sysgaming", {
+    font: "ANSI Shadow",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
   });
+
+  console.info("Nome do projeto:", green(answers.projectName));
+  console.info("API REST:", blue(answers.restApiUrl));
+  console.info("API Graphql:", magenta(answers.restApiUrl));
+});
